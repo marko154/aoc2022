d2adc087b6a46809508b14b10405d30089e5f5ee
marko154 <marko.gartnar777@gmail.com>
2022-12-01 06:12:39 +0100

    day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,15 @@
+lines = [line.strip() for line in open("./input.txt", "r").readlines()]
 
+elves = []
+elf = 0
 
+for line in lines:
+    if line:
+        elf += int(line)
+    else:
+        elves.append(elf)
+        elf = 0
+
+elves.sort(reverse=True)
+print(elves[0])
+print(sum(elves[:3]))
