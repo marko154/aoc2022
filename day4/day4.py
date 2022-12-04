9c850486dc01e27caab4f20b721fdb80ea8c7bd3
marko154 <marko.gartnar777@gmail.com>
2022-12-04 07:06:39 +0100

    day4 non retarded version

--- day4/day4.py
+++ day4/day4.py
@@ -5,17 +5,16 @@ ans2 = 0
 
 for line in lines:
     a, b = line.split(",")
+    s1, e1 = [int(n) for n in a.split("-")]
+    s2, e2 = [int(n) for n in b.split("-")]
 
-    r1 = [int(n) for n in a.split("-")]
-    r2 = [int(n) for n in b.split("-")]
+    start = max(s1, s2)
+    end = min(e1, e2)
 
-    s1 = set(range(r1[0], r1[1] + 1))
-    s2 = set(range(r2[0], r2[1] + 1))
-
-    if len(s1 & s2) == min(len(s1), len(s2)):
+    if (start, end) in ((s1, e1), (s2, e2)):
         ans += 1
 
-    if len(s1 & s2) > 0:
+    if start <= end:
         ans2 += 1
 
 print(ans)
