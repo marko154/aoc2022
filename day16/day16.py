46f2e8323ddba22f7345544089d26807e7f6011e
marko154 <marko.gartnar777@gmail.com>
2022-12-16 17:01:09 +0100

    part1 with floyd warshal

--- day16/day16.py
+++ day16/day16.py
@@ -3,15 +3,16 @@ import networkx as nx
 import re
 
 lines = [line.strip() for line in open("./input.txt", "r").readlines()]
-lines = [line.strip() for line in open("./test.txt", "r").readlines()]
+# lines = [line.strip() for line in open("./test.txt", "r").readlines()]
 
-valves = [0] * len(lines)
+valves = [[]] * len(lines)
 matrix = [[float("inf")] * len(lines) for x in range(len(lines))]
 mapping = {}
 i = 0
 
 for line in lines:
     v, *exits = re.findall(r"[A-Z][A-Z]", line)
+    rate = int(re.findall(r"\d+", line)[0])
     if v not in mapping:
         mapping[v] = i
         i += 1
@@ -20,61 +21,69 @@ for line in lines:
             mapping[exit] = i
             i += 1
         matrix[mapping[v]][mapping[exit]] = 1
-    rate = int(re.findall(r"\-?\d+", line)[0])
-    valves[v] = [rate, exits]
+    valves[mapping[v]] = [v, rate, exits]
 
-edges = []
-
-for valve in valves:
-    rate, exits = valves[valve]
-    for exit in exits:
-        rate2, _ = valves[exit]
-        edges.append((valve + str(rate), exit + str(rate2)))
-
-
-for valve in valves:
-    index = valve
-    for
-    matrix[]
-
-G = nx.DiGraph()
-G.add_edges_from(edges)
-nx.draw_networkx(G)
-plt.show()
-exit()
-
-ans = 0
+for via in range(len(matrix)):
+    for fr in range(len(matrix)):
+        for to in range(len(matrix)):
+            matrix[fr][to] = min(matrix[fr][to], matrix[fr][via] + matrix[via][to])
 
 opened = set()
 DP = {}
-print(valves)
+
+nodes = [i for i in range(len(matrix)) if valves[i][0] == "AA" or valves[i][1] > 0]
 
 
 def dfs(valve, time):
-    global opened, valves
-    if time <= 0:
+    global opened, valves, nodes
+    if time <= 0 or len(opened) == len(nodes) - 1:
         return 0
-    rate, exits = valves[valve]
-    tupled = tuple(sorted(opened))
-    if (valve, time, *tupled) in DP:
-        return DP[(valve, time, *tupled)]
+    name, rate, _ = valves[valve]
+    key = (time, valve, *tuple(sorted(opened)))
+    if key in DP:
+        return DP[key]
     max_pressure = 0
 
-    for exit in exits:
-        without = dfs(exit, time - 1)
+    for exit in nodes:
+        dist = matrix[valve][exit]
+        if exit == valve or dist == float("inf"):
+            continue
+
+        without = dfs(exit, time - dist)
         max_pressure = max(max_pressure, without)
         # if already open dont open again
-        if valve not in opened or rate == 0:
+
+        if valve not in opened and rate != 0:
             opened.add(valve)
-            with_ = dfs(exit, time - 2)
+            with_ = dfs(exit, time - dist - 1)
             max_pressure = max(max_pressure, with_ + time * rate)
             if valve in opened:
                 opened.remove(valve)
-    DP[(valve, time, *tupled)] = max_pressure
+    DP[key] = max_pressure
     return max_pressure
 
 
-best_pressure = dfs("AA", 29)
-ans = max(ans, best_pressure)
+best_pressure = dfs(mapping["AA"], 29)
+print(best_pressure)
 
-print(ans)
+
+# G = nx.DiGraph()
+
+
+# for node in nodes:
+#     for inode in nodes:
+#         dist = matrix[node][inode]
+#         if inode == node or dist > 9999999:
+#             continue
+#         G.add_edge(
+#             valves[node][0] + str(valves[node][1]),
+#             valves[inode][0] + str(valves[node][1]),
+#             weight=dist,
+#         )
+
+# pos = nx.get_node_attributes(G, "pos")
+# pos = nx.spring_layout(G)  # pos = nx.nx_agraph.graphviz_layout(G)
+# nx.draw_networkx(G, pos)
+# # labels = nx.get_edge_attributes(G, "weight")
+# # nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+# plt.show()
