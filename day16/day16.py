e2737a5fece6c96c5c8f940c1a81b16cc7d1509f
Marko Gartnar <marko@Markos-MacBook-Air.local>
2022-12-16 23:38:43 +0100

    i did it

--- day16/day16.py
+++ day16/day16.py
@@ -1,11 +1,6 @@
-import matplotlib.pyplot as plt
-import networkx as nx
 import re
-import time as Time
-from itertools import permutations
 
 lines = [line.strip() for line in open("./input.txt", "r").readlines()]
-lines = [line.strip() for line in open("./test.txt", "r").readlines()]
 
 valves = [[]] * len(lines)
 matrix = [[float("inf")] * len(lines) for x in range(len(lines))]
@@ -31,36 +26,17 @@ for via in range(len(matrix)):
             matrix[fr][to] = min(matrix[fr][to], matrix[fr]
                                  [via] + matrix[via][to])
 
-
-# nodes = [i for i in range(len(matrix)) if valves[i][1] > 0]
-
-# best = 0
-# for permutation in permutations(nodes):
-#     score = 0
-#     time = 30
-#     valve = mapping["AA"]
-#     for node in permutation:
-#         time -= matrix[valve][node] + 1
-#         score += time * valves[node][1]
-#         valve = node
-#     best = max(best, score)
-
-# print(best)
-
-visited = set()
-DP = {}
-
 nodes = [i for i in range(len(matrix)) if valves[i][0]
          == "AA" or valves[i][1] > 0]
+nodes.sort(key=lambda node: valves[node][0])
+visited = set()
 
 
-def dfs(valve, time):
-    global visited, valves, nodes
+def dfs(valve, time, nodes, visited):
+    global valves
     if time <= 0:
         return 0
-    # key = (valve, time, tuple(sorted(visited)))
-    # if key in DP:
-    #     return DP[key]
+
     name, rate, _ = valves[valve]
     visited.add(valve)
     max_pressure = 0
@@ -68,45 +44,31 @@ def dfs(valve, time):
     for exit in nodes:
         dist = matrix[valve][exit]
         if exit not in visited:
-            next_time = time - dist - 1
-            pressure = dfs(exit, next_time)
+            pressure = dfs(exit, time - dist - 1, nodes, visited)
             max_pressure = max(max_pressure, pressure)
 
-    # DP[key] = max_pressure
     visited.discard(valve)
     return max_pressure + time * rate
 
 
-best_pressure = dfs(mapping["AA"], 30)
-print(best_pressure)
+done = set()
+part2 = 0
+for i in range(1 << len(nodes) - 1):
+    print(i)
+    if i in done:
+        continue
+    left = []
+    right = []
+    for j in range(len(nodes) - 1):
+        if (1 << j) & i:
+            left.append(nodes[1 + j])
+        else:
+            right.append(nodes[1 + j])
+    start = mapping["AA"]
+    left_pressure = dfs(start, 26, left, set())
+    right_pressure = dfs(start, 26, right, set())
+    done.add(i)
+    done.add(~i)
+    part2 = max(part2, left_pressure + right_pressure)
 
-
-# for i, line in enumerate(matrix):
-#     if i == 0:
-#         print("    ", end="")
-#         for j in range(len(matrix)):
-#             print(valves[j][0], end=" ")
-#         print()
-#     print(valves[i][0], end=" ")
-#     print(line)
-
-# G = nx.DiGraph()
-
-
-# for node in nodes:
-#     for inode in nodes:
-#         dist = matrix[node][inode]
-#         if inode == node or dist > 9999999:
-#             continue
-#         G.add_edge(
-#             valves[node][0] + str(valves[node][1]),
-#             valves[inode][0] + str(valves[inode][1]),
-#             weight=dist,
-#         )
-
-# pos = nx.get_node_attributes(G, "pos")
-# pos = nx.spring_layout(G)  # pos = nx.nx_agraph.graphviz_layout(G)
-# nx.draw_networkx(G, pos)
-# labels = nx.get_edge_attributes(G, "weight")
-# nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-# plt.show()
+print(part2)
