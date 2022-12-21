2d28516c96da1503bb0156cb6ec84323c16586b4
marko154 <marko.gartnar777@gmail.com>
2022-12-21 10:47:00 +0100

    day21

--- day19/day19.py
+++ day19/day19.py
@@ -1,200 +1,126 @@
 import re  # search, findall, replace
-from collections import defaultdict, deque, Counter, OrderedDict
-import heapq
-import bisect
-from itertools import combinations, combinations_with_replacement, permutations, product
 import sys
-from time import sleep
 
 filename = "./input.txt" if len(sys.argv) < 2 else sys.argv[1]
 lines = [line.strip() for line in open(filename, "r").readlines()]
 
-
-# ore, clay, obsidian, geode
-TEMP = 0
-
-
-def pick_robot(costs, robots, rocks):
-    # if can build a geode robot return geode
-    if rocks["ore"] >= costs["geode"][0] and rocks["obsidian"] >= costs["geode"][1]:
-        return "geode"
-    # else
-    #
-
-    pass
-
-
-# Each ore robot costs 4 ore.
-# Each clay robot costs 2 ore.
-# Each obsidian robot costs 3 ore and 14 clay.
-# Each geode robot costs 2 ore and 7 obsidian.
-
-
-def dfs(time, costs, robots, rocks):
-    global TEMP
-    if time <= 0:
-        if rocks["geode"] > TEMP:
-            TEMP = rocks["geode"]
-            print(TEMP)
-        return rocks["geode"]
-
-    possibles = []
-    best = 0
-    was_geode = False
-    time_left = time
-    max_geodes = time_left * (time_left + 1) // 2
-
-    sleep(1)
-
-    print(time, robots, rocks)
-
-    # geode
-    if rocks["ore"] >= costs["geode"][0] and rocks["obsidian"] >= costs["geode"][1]:
-        new_robots = robots.copy()
-        new_robots["geode"] += 1
-        new_rocks = create_rocks()
-        new_rocks["ore"] -= costs["geode"][0]
-        new_rocks["obsidian"] -= costs["geode"][1]
-        possible = dfs(time - 1, costs, new_robots, new_rocks)
-        possibles.append(possible)
-        best = max(possible, best)
-        was_geode = True
-    # obsidian
-    if best >= max_geodes:
-        # print(max_geodes)
-        return best
-
-    max_obsidian = costs["geode"][1]
-    if (
-        rocks["ore"] >= costs["obsidian"][0]
-        and rocks["clay"] >= costs["obsidian"][1]
-        and robots["obsidian"] < max_obsidian
-    ):
-        new_robots = robots.copy()
-        new_robots["obsidian"] += 1
-        new_rocks = create_rocks()
-        new_rocks["ore"] -= costs["obsidian"][0]
-        new_rocks["clay"] -= costs["obsidian"][1]
-        possible = dfs(time - 1, costs, new_robots, new_rocks)
-        possibles.append(possible)
-        best = max(possible, best)
-    # clay
-    max_clay = costs["obsidian"][1]
-    if rocks["ore"] >= costs["clay"][0] and robots["clay"] < max_clay:
-        new_robots = robots.copy()
-        new_robots["clay"] += 1
-        new_rocks = create_rocks()
-        new_rocks["ore"] -= costs["clay"][0]
-        possible = dfs(time - 1, costs, new_robots, new_rocks)
-        possibles.append(possible)
-        best = max(possible, best)
-
-    # ore
-    max_ore = max(x[0] for x in costs.values())
-    if rocks["ore"] >= costs["ore"][0] and robots["ore"] < max_ore:
-        new_robots = robots.copy()
-        new_robots["ore"] += 1
-        new_rocks = create_rocks()
-        new_rocks["ore"] -= costs["ore"][0]
-        possible = dfs(time - 1, costs, new_robots, new_rocks)
-        possibles.append(possible)
-        best = max(possible, best)
-
-    # if no robots were created
-    if not was_geode:
-        possible = dfs(time - 1, costs, robots, create_rocks())
-        possibles.append(possible)
-        best = max(possible, best)
-
-    return best
-
-
-ans = 0
+part1 = 0
 names = ["ore", "clay", "obsidian", "geode"]
 
 
-for i, line in enumerate(lines):
-    robots = line.split(": ")[1].split(". ")
-    costs = {}
-    for j, robot in enumerate(robots):
-        costs[names[j]] = tuple(map(int, re.findall(r"\d+", robot)))
-    robots = {"ore": 1, "clay": 0, "obsidian": 0, "geode": 0}
-    rocks = {"ore": 0, "clay": 0, "obsidian": 0, "geode": 0}
-    # best = dfs(24, costs, robots, rocks)
+def solve(end, oreC, clayC, obsC, geodeC):
+    init_robots = (1, 0, 0, 0)
+    init_rocks = (0, 0, 0, 0)
+    Q = []
+    seen = set()
+    Q.append((0, init_robots, init_rocks))
+
+    max_obsidian = geodeC[1]
+    max_clay = obsC[1]
+    max_ore = max(oreC, clayC, obsC[0], geodeC[0])
+
     best = 0
+    bestGR = 0
 
-    Q = deque()
-    Q.append((1, robots, rocks))
-
-    max_obsidian = costs["geode"][1]
-    max_clay = costs["obsidian"][1]
-    max_ore = max(x[0] for x in costs.values())
-    best_so_far = None
-    ans = 0
     while Q:
-        time, robots, rocks = Q.popleft()
-        ans = max(ans, rocks["geode"])
-        if robots["geode"] > 0:
-            if not best_so_far or best_so_far[1]["geode"] < robots["geode"]:
-                best_so_far = (time, robots, rocks)
-            print(time, len(Q), rocks, robots)
-        if best_so_far and best_so_far[1]["geode"] < robots["geode"]:
-            continue
-        if time == 24 or len(Q) > 7_000_000:
-            print(len(Q))
-            break
-        was_geode = False
-        next_rocks = rocks.copy()
-        for rock in robots:
-            next_rocks[rock] += robots[rock]
+        state = Q.pop()
+        time, robots, rocks = state
+        ore, clay, obs, geode = rocks
+        oreR, clayR, obsR, geodeR = robots
+        best = max(best, geode)
+        bestGR = max(bestGR, geodeR)
 
-        if rocks["ore"] >= costs["geode"][0] and rocks["obsidian"] >= costs["geode"][1]:
-            new_robots = robots.copy()
-            new_robots["geode"] += 1
-            new_rocks = next_rocks.copy()
-            new_rocks["ore"] -= costs["geode"][0]
-            new_rocks["obsidian"] -= costs["geode"][1]
-            Q.append((time + 1, new_robots, new_rocks))
-            was_geode = True
+        if time >= end:
+            continue
+
+        oreR = min(max_ore, oreR)
+        clayR = min(max_clay, clayR)
+        obsR = min(max_obsidian, obsR)
+
+        # remove rocks if you can never spend them to get better memoization
+        ore = min((end - time) * max_ore - (end - 1 - time) * oreR, ore)
+        clay = min((end - time) * max_clay - (end - 1 - time) * clayR, clay)
+        obs = min((end - time) * max_obsidian - (end - 1 - time) * obsR, obs)
+
+        key = (time, (oreR, clayR, obsR, geodeR), (ore, clay, obs, geode))
+        if key in seen:
+            continue
+
+        seen.add(key)
+
+        n_ore = ore + oreR
+        n_clay = clay + clayR
+        n_obs = obs + obsR
+        n_geode = geode + geodeR
+        qsize = len(Q)
+
+        # geode
+        if ore >= geodeC[0] and obs >= geodeC[1]:
+            Q.append(
+                (
+                    time + 1,
+                    (oreR, clayR, obsR, geodeR + 1),
+                    (n_ore - geodeC[0], n_clay, n_obs - geodeC[1], n_geode),
+                )
+            )
+            continue
         # obsidian
-        if (
-            rocks["ore"] >= costs["obsidian"][0]
-            and rocks["clay"] >= costs["obsidian"][1]
-            and robots["obsidian"] < max_obsidian
-        ):
-            new_robots = robots.copy()
-            new_robots["obsidian"] += 1
-            new_rocks = next_rocks.copy()
-            new_rocks["ore"] -= costs["obsidian"][0]
-            new_rocks["clay"] -= costs["obsidian"][1]
-            if not was_geode:
-                Q.append((time + 1, new_robots, new_rocks))
+        if ore >= obsC[0] and clay >= obsC[1]:
+            Q.append(
+                (
+                    time + 1,
+                    (oreR, clayR, obsR + 1, geodeR),
+                    (n_ore - obsC[0], n_clay - obsC[1], n_obs, n_geode),
+                )
+            )
         # clay
-        if rocks["ore"] >= costs["clay"][0] and robots["clay"] < max_clay:
-            new_robots = robots.copy()
-            new_robots["clay"] += 1
-            new_rocks = next_rocks.copy()
-            new_rocks["ore"] -= costs["clay"][0]
-            if not was_geode:
-                Q.append((time + 1, new_robots, new_rocks))
+        if ore >= clayC:
+            Q.append(
+                (
+                    time + 1,
+                    (oreR, clayR + 1, obsR, geodeR),
+                    (n_ore - clayC, n_clay, n_obs, n_geode),
+                )
+            )
 
         # ore
-        if rocks["ore"] >= costs["ore"][0] and robots["ore"] < max_ore:
-            new_robots = robots.copy()
-            new_robots["ore"] += 1
-            new_rocks = next_rocks.copy()
-            new_rocks["ore"] -= costs["ore"][0]
-            if not was_geode:
-                Q.append((time + 1, new_robots, new_rocks))
+        if ore >= oreC:
+            Q.append(
+                (
+                    time + 1,
+                    (oreR + 1, clayR, obsR, geodeR),
+                    (n_ore - oreC, n_clay, n_obs, n_geode),
+                )
+            )
 
-        # if no robots were created
-        if not was_geode:
-            Q.append((time + 1, robots, next_rocks))
+        if qsize + 4 != len(Q):
+            Q.append(
+                (
+                    time + 1,
+                    (oreR, clayR, obsR, geodeR),
+                    (n_ore, n_clay, n_obs, n_geode),
+                )
+            )
+    return best
 
-    print(costs, robots, rocks)
-    print(ans, "!" * 20)
-    break
-    ans += (i + 1) * best
+
+part1 = 0
+part2 = 1
+
+for i, line in enumerate(lines):
+    data = line.split(": ")[1].split(". ")
+    parsed_costs = [tuple(map(int, re.findall(r"\d+", robot))) for robot in data]
+
+    oreC = parsed_costs[0][0]
+    clayC = parsed_costs[1][0]
+    obsC = (parsed_costs[2][0], parsed_costs[2][1])
+    geodeC = (parsed_costs[3][0], parsed_costs[3][1])
+    if i < 3:
+        part2 *= solve(32, oreC, clayC, obsC, geodeC)
+
+    best = solve(24, oreC, clayC, obsC, geodeC)
+    part1 += (i + 1) * best
 
 
-print(ans)
+print(part1)
+print(part2)
---
+++ day21/day21.py
@@ -0,0 +1,80 @@
+import sys
+
+
+filename = "./input.txt" if len(sys.argv) < 2 else sys.argv[1]
+lines = [line.strip() for line in open(filename, "r").readlines()]
+
+tree = {}
+
+for line in lines:
+    name, action = line.split(": ")
+    tree[name] = action.split()
+
+
+def evaluate(node):
+    global tree
+
+    if len(tree[node]) == 1:
+        return int(tree[node][0])
+    left, op, right = tree[node]
+    return eval(f"{evaluate(left)} {op} {evaluate(right)}")
+
+
+print(int(evaluate("root")))
+
+
+marked = {}
+
+
+def traverse(node):
+    global tree, marked
+
+    if len(tree[node]) == 1:
+        marked[node] = (int(tree[node][0]), node == "humn")
+        return marked[node]
+
+    left, op, right = tree[node]
+    left, has_humn_l = traverse(left)
+    right, has_humn_r = traverse(right)
+    has_human = has_humn_r or has_humn_l
+    value = eval(f"{left} {op} {right}")
+    marked[node] = (value, has_human)
+    return marked[node]
+
+
+traverse("root")
+
+node = "root"
+left, op, right = tree[node]
+l_val, has_humn_l = marked[left]
+r_val, has_humn_r = marked[right]
+
+node = left if has_humn_l else right
+value = l_val if has_humn_r else r_val
+
+while node != "humn":
+    left, op, right = tree[node]
+    l_val, has_humn_l = marked[left]
+    r_val, _ = marked[right]
+
+    if has_humn_l:
+        if op == "+":
+            value -= r_val
+        elif op == "-":
+            value += r_val
+        elif op == "*":
+            value /= r_val
+        elif op == "/":
+            value *= r_val
+    else:
+        if op == "+":
+            value -= l_val
+        elif op == "-":
+            value = l_val - value
+        elif op == "*":
+            value /= l_val
+        elif op == "/":
+            value = l_val / value
+    node = left if has_humn_l else right
+
+print(int(value))
