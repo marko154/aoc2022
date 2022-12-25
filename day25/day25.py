a348913d922837e040c3779359f041059b0e7a08
marko154 <marko.gartnar777@gmail.com>
2022-12-25 06:41:27 +0100

    .

--- day25/day25.py
+++ day25/day25.py
@@ -1,11 +1,3 @@
-import re  # search, findall, replace
-from collections import defaultdict, deque, Counter, OrderedDict
-from sortedcontainers import SortedDict, SortedSet, SortedList
-import heapq
-import bisect
-from itertools import combinations, combinations_with_replacement, permutations, product
-import math
-import numpy as np
 import sys
 
 filename = "./input.txt" if len(sys.argv) < 2 else sys.argv[1]
